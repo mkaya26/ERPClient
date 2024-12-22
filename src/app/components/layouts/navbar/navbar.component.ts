67ebce5ddf9d872f94ac9530b68c73e0f14eeb6c
Murat Kaya <91426358+mkaya26@users.noreply.github.com>
2024-12-22 04:00:02 +0300

    logout işlemi düzeltildi.

--- src/app/components/layouts/navbar/navbar.component.ts
+++ src/app/components/layouts/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(
+    private router: Router
+  ) { }
+
+  logout() {
+    localStorage.removeItem("token");
+    this.router.navigateByUrl("/login");
+  }
 }
